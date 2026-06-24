8c89f9c49fe7f552e3c4838f6e950bbc96009f50
BABABABA <steffen.flaskamp@bk-beckum.eu>
2026-06-24 11:01:46 +0200

    duplicate messages fixx

--- proxy/src/main/java/org/pytenix/chat/listener/SystemChatPacketListener.java
+++ proxy/src/main/java/org/pytenix/chat/listener/SystemChatPacketListener.java
@@ -20,63 +20,56 @@ public class SystemChatPacketListener implements PacketListener {
     private final SystemChatModule systemChatService;
     private final MessageSequencer messageSequencer;
 
-
     @Override
     public void onPacketSend(PacketSendEvent event) {
-        if (event.getPacketType() == PacketType.Play.Server.SYSTEM_CHAT_MESSAGE) {
-            if (event.isCancelled() || !systemChatService.isModuleActive()) return;
+        if (event.getPacketType() != PacketType.Play.Server.SYSTEM_CHAT_MESSAGE) return;
+        if (event.isCancelled() || !systemChatService.isModuleActive()) return;
 
-            UUID uuid = event.getUser().getUUID();
-            if (uuid == null) return;
+        UUID uuid = event.getUser().getUUID();
+        if (uuid == null) return;
 
-            Player player = systemChatService.getTranslatorPlugin().getProxyServer().getPlayer(uuid).orElse(null);
-            if (player == null) return;
+        Player player = systemChatService.getTranslatorPlugin().getProxyServer().getPlayer(uuid).orElse(null);
+        if (player == null) return;
 
+        if (!systemChatService.checkIfNeed(uuid)) return;
 
-            if (!systemChatService.checkIfNeed(uuid)) return;
+        WrapperPlayServerSystemChatMessage packet = new WrapperPlayServerSystemChatMessage(event);
+        boolean isOverlay = packet.isOverlay();
 
-            WrapperPlayServerSystemChatMessage packet = new WrapperPlayServerSystemChatMessage(event);
-            boolean isOverlay = packet.isOverlay();
+        if (isOverlay) return;
 
-            if (isOverlay) return;
+        Component messageComponent = packet.getMessage();
+        String rawText = LegacyComponentSerializer.legacySection().serialize(messageComponent);
 
-            Component messageComponent = packet.getMessage();
-            String rawText = LegacyComponentSerializer.legacySection().serialize(messageComponent);
-
-            if (rawText.contains("Can't deliver chat message") ||
-                    rawText.contains("kann nicht zugestellt werden") ||
-                    rawText.contains("multiplayer.message_not_delivered")
-            ) {
-                event.setCancelled(true);
-                return;
-            }
-
-            if (messageSequencer.isIgnored(uuid, messageComponent))
-                return;
-
-
-            //TODO: REFACTOR!!!!
-
-            systemChatService.getTranslatorPlugin().getProfileSocketEndpoint().getProfile(uuid)
-                    .thenAcceptAsync(profileData ->
-                    {
-                        if (profileData.consentType() == NetworkPackets.ProfilePacket.ConsentType.DECLINED)
-                            return;
-
-                        event.setCancelled(true);
-
-
-                        messageSequencer.translateWithOrder(
-                                uuid,
-                                messageComponent,
-                                rawText,
-                                systemChatService.getPlayerLocaleProcessor().retrieveLocale(uuid),
-                                isOverlay
-                        );
-
-                    });
-
-
+        if (rawText.contains("Can't deliver chat message") ||
+                rawText.contains("kann nicht zugestellt werden") ||
+                rawText.contains("multiplayer.message_not_delivered")
+        ) {
+            event.setCancelled(true);
+            return;
         }
+
+        if (messageSequencer.isIgnored(uuid, messageComponent)) return;
+
+
+        event.setCancelled(true);
+
+        systemChatService.getTranslatorPlugin().getProfileSocketEndpoint().getProfile(uuid)
+                .thenAcceptAsync(profileData -> {
+
+                    if (profileData.consentType() == NetworkPackets.ProfilePacket.ConsentType.DECLINED) {
+                        messageSequencer.ignoreNextMessage(uuid, messageComponent);
+                        player.sendMessage(messageComponent);
+                        return;
+                    }
+
+                    messageSequencer.translateWithOrder(
+                            uuid,
+                            messageComponent,
+                            rawText,
+                            systemChatService.getPlayerLocaleProcessor().retrieveLocale(uuid),
+                            isOverlay
+                    );
+                });
     }
 }
